51d6e00c168a27eda37e943701ec35892af61856
faisalsaificode <faisalsaifi.code@gmail.com>
2025-10-17 13:24:05 +0530

    commit

--- src/features/cartItems/cartItems.controller.js
+++ src/features/cartItems/cartItems.controller.js
@@ -1,42 +1,47 @@
-// src/features/cartItems/cartItems.controller.js
-import CartItemModel from "./cartItems.model.js"; // if you're still using it for get/delete
+import CartItemModel from "./cartItems.model.js";
 import CartItemsRepository from "./cartItems.repository.js";
-import { ApplicationError } from "../../error-handler/applicationError.js";
 
 export class CartItemsController {
-  constructor() {
-    this.cartItemsRepository = new CartItemsRepository();
-  }
-
-  async add(req, res, next) {
-    try {
-      // Prefer body for POST
-      const { productID, quantity } = req.body || {};
-      const userID = req.user?.id || req.userID;
-
-      if (!userID) throw new ApplicationError("Unauthorized", 401);
-      if (!productID) throw new ApplicationError("productID is required", 400);
-
-      const item = await this.cartItemsRepository.add(productID, userID, quantity);
-      return res.status(201).json({ success: true, data: item });
-    } catch (err) {
-      // Let global error middleware format this if you have it
-      const status = err?.statusCode || 500;
-      return res.status(status).json({ success: false, message: err.message || "Something went wrong" });
+    
+    constructor(){
+        this.cartItemsRepository = new CartItemsRepository();
     }
-  }
 
-  get(req, res) {
-    const userID = req.user?.id || req.userID;
-    const items = CartItemModel.get(userID);
-    return res.status(200).send(items);
-  }
+    async add(req, res) {
+        try{
+            const { productID, quantity } = req.body;
+            const userID = req.userID;
+            await this.cartItemsRepository.add(productID, userID, quantity);
+            res.status(201).send("Cart is updated");
+        }catch(err){
+            console.log(err);
+            return res.status(200).send("Something went wrong");
+           }    
+    }
 
-  delete(req, res) {
-    const userID = req.user?.id || req.userID;
-    const cartItemID = req.params.id;
-    const error = CartItemModel.delete(cartItemID, userID);
-    if (error) return res.status(404).send(error);
-    return res.status(200).send("Cart item is removed");
-  }
+    async get(req, res){
+        try{
+            const userID = req.userID;
+            const items = await this.cartItemsRepository.get(userID);
+            return res.status(200).send(items);
+        }catch(err){
+            console.log(err);
+            return res.status(200).send("Something went wrong");
+           }   
+    }
+
+    async delete(req, res) {
+        const userID = req.userID;
+        const cartItemID = req.params.id;
+        const isDeleted = await this.cartItemsRepository.delete(
+            userID,
+            cartItemID
+        );
+        if (!isDeleted) {
+            return res.status(404).send("Item not found");
+        }
+        return res
+        .status(200)
+        .send('Cart item is removed');
+    }
 }
--- src/features/cartItems/cartItems.repository.js
+++ src/features/cartItems/cartItems.repository.js
@@ -1,31 +1,43 @@
-// src/features/cartItems/cartItems.repository.js
-import { ApplicationError } from "../../error-handler/applicationError.js";
-import { getDB } from "../../config/mongodb.js";
-import { ObjectId } from "mongodb";
+import { ObjectId } from 'mongodb';
+import { getDB } from '../../config/mongodb.js';
 
-export default class CartItemsRepository {
-  constructor() {
-    this.collection = "cartItems"; // <-- use a cart-specific collection
-  }
-
-  async add(productID, userID, quantity = 1) {
-    try {
-      const db = getDB();
-      const collection = db.collection(this.collection);
-
-      const newCartItem = {
-        productID: new ObjectId(productID),
-        userID: new ObjectId(userID),
-        quantity: Number(quantity) || 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-
-      const result = await collection.insertOne(newCartItem);
-      return { ...newCartItem, _id: result.insertedId };
-    } catch (err) {
-      console.log(err);
-      throw new ApplicationError("Something went wrong with database", 500);
+export default class CartItemsRepository{
+    constructor(){
+        this.collection = "cartItems";
     }
-  }
+
+
+    async add(productID, userID, quantity){
+        try{
+        const db = getDB();
+        const collection = db.collection(this.collection);
+        await collection.insertOne({productID: new ObjectId(productID), userID: new ObjectId(userID), quantity}) 
+        }catch(err){
+            console.log(err);
+            throw new ApplicationError("Something went wrong with database", 500);
+        }
+    }
+
+    async get(userID){
+        try{
+        const db = getDB();
+        const collection = db.collection(this.collection);
+        return await collection.find({userID: new ObjectId(userID)}).toArray();
+        }catch(err){
+            console.log(err);
+            throw new ApplicationError("Something went wrong with database", 500);
+        }
+    }
+
+    async delete(userID, cartItemID){
+        try{
+        const db = getDB();
+        const collection = db.collection(this.collection);
+        const result = await collection.deleteOne({_id: new ObjectId(cartItemID), userID:  new ObjectId(userID)});
+        return result.deletedCount>0;
+        }catch(err){
+            console.log(err);
+            throw new ApplicationError("Something went wrong with database", 500);
+        }
+    }
 }
